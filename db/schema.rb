f7c0b0fdadf888f9b40b87e6dbff33a7b3a631ed
machida <machida@fjord.jp>
2025-12-02 03:11:22 +0900

    chore: sync schema to rails 8.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_090349) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_31_090349) do
   create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.string "user_id"
-    t.string "password_digest"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email"
     t.datetime "password_changed_at"
-    t.string "password_reset_token"
+    t.string "password_digest"
     t.datetime "password_reset_sent_at"
+    t.string "password_reset_token"
     t.string "theme_color", default: "sky", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_id"
     t.index ["password_reset_token"], name: "index_admins_on_password_reset_token", unique: true
   end
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.text "summary"
-    t.string "thumbnail"
     t.string "author"
+    t.text "body"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "draft", default: false, null: false
     t.datetime "published_at"
+    t.text "summary"
+    t.string "thumbnail"
+    t.string "title"
+    t.datetime "updated_at", null: false
   end
 
   create_table "site_settings", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "value", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
+    t.text "value", null: false
     t.index ["name"], name: "index_site_settings_on_name", unique: true
   end
 end
